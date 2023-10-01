df4c3cc37631e159f4900e2f318fd0816b2cdd82
MJ <mingkywill@gmail.com>
2023-10-01 22:54:42 +0900

    more examples

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
 "use strict";
 
-class UserData {
-  loginUser(id, password) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (
-          (id === "ellie" && password == "dream") ||
-          (id === "coder" && password == "academy")
-        ) {
-          resolve(id);
-        } else {
-          reject(new Error("not found"));
-        }
-      }, 2000);
-    });
-  }
+// class UserData {
+//   loginUser(id, password) {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         if (
+//           (id === "ellie" && password == "dream") ||
+//           (id === "coder" && password == "academy")
+//         ) {
+//           resolve(id);
+//         } else {
+//           reject(new Error("not found"));
+//         }
+//       }, 2000);
+//     });
+//   }
 
-  getRoles(user) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (user === "ellie") {
-          resolve({ name: "ellie", role: "admin" });
-        } else {
-          reject(new Error("not found"));
-        }
-      }, 2000);
-    });
-  }
-}
+//   getRoles(user) {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         if (user === "ellie") {
+//           resolve({ name: "ellie", role: "admin" });
+//         } else {
+//           reject(new Error("not found"));
+//         }
+//       }, 2000);
+//     });
+//   }
+// }
 
-const userStorage = new UserData();
-const id = prompt("enter your id");
-const password = prompt("enter your password");
-// userStorage
-//   .loginUser(id, password)
-//   .then(userStorage.getRoles)
-//   .then((user) => alert(`Hello ${user.name}, you have a ${user.role} role`));
+// const userStorage = new UserData();
+// const id = prompt("enter your id");
+// const password = prompt("enter your password");
+// // userStorage
+// //   .loginUser(id, password)
+// //   .then(userStorage.getRoles)
+// //   .then((user) => alert(`Hello ${user.name}, you have a ${user.role} role`));
 
-async function userCheck() {
-  try {
-    const userInfo = await userStorage.loginUser(id, password);
-    const user = await userStorage.getRoles(userInfo);
+// async function userCheck() {
+//   try {
+//     const userInfo = await userStorage.loginUser(id, password);
+//     const user = await userStorage.getRoles(userInfo);
 
-    alert(`Hello ${user.name}, you have a ${user.role} role`);
-  } catch (error) {
-    console.log(error);
-  }
-}
+//     alert(`Hello ${user.name}, you have a ${user.role} role`);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
 //1. async & await
 function fetchUser1() {
@@ -124,3 +124,29 @@ function pickOnlyOne() {
 }
 
 pickOnlyOne().then(console.log);
+
+//consuming promises with async/await
+function getPosition() {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
+
+async function whereAmI() {
+  const pos = await getPosition();
+  const { latitude: lat, longitude: lng } = pos.coords;
+
+  console.log(pos);
+
+  const resGeo = await fetch(
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`
+  );
+
+  const dataGeo = await resGeo.json();
+  const res = await fetch(
+    `https://restcountries.com/v3.1/alpha/${dataGeo.countryCode}`
+  );
+  const data = await res.json();
+  console.log(data);
+}
+whereAmI();
